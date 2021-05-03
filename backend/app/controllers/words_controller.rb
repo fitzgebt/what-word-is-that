b67d32b753eec5665dbdceaaad576397a70ab715
fitzgebt <fitzgebt@gmail.com>
2021-05-03 14:45:19 -0700

    created actions for words_controller

--- backend/app/controllers/words_controller.rb
+++ backend/app/controllers/words_controller.rb
@@ -1,2 +1,21 @@
 class WordsController < ApplicationController
+    def index
+        render json: Word.all
+    end
+
+    def show
+        word = Word.find_by(id: params[:id])
+        render json: word
+    end
+
+    def create
+        # binding.pry
+        word = Word.create(word_params)
+        render json: word
+        # params
+    end
+
+    def word_params
+        params.require(:word).permit(:id, :name, :round_id)
+    end
 end
