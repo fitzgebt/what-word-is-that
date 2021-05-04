5e4baa958ac2e5ca61e6bbb8d2c1fb985eaf5b7b
fitzgebt <fitzgebt@gmail.com>
2021-05-04 12:38:51 -0700

    TLD push to match previous push to 'backend'

--- frontend/src/round.js
+++ frontend/src/round.js
@@ -10,14 +10,13 @@ function fetchRounds() {
 function appendRounds(rounds){
     const roundDiv = document.getElementById("pastRounds")
     for (let round of rounds) {
-        debugger
         const li = document.createElement("li")
         let wl
         (round.win) ? wl = "WIN" : wl = "LOSS";
         if (round.complete) {
             li.innerText = `Round ${round.id} -  Word: ${round.word.name}  -  ${wl}!`
         } else {
-            li.innerText = `Round ${round.id}`
+            li.innerText = `Round ${round.id} - In Progress...`
         }
         roundDiv.append(li)
     }
@@ -25,17 +24,29 @@ function appendRounds(rounds){
 
 function postRound(e) {
     e.preventDefault()
+    const body = {
+        round: {
+            win: false,
+            complete: false,
+            guesses: 0,
+            word_id: 0,
+            word: {
+                id: 0,
+                name: ""
+            }
+        }
+    }
     const options = {
         method: "POST",
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify({round: {win: false, complete: false} })
+        body: JSON.stringify(body)
     }
     fetch("http://localhost:3000/rounds", options)
     .then(r => r.json())
     .then(round => console.log(round))
-    fetchWord()
+    debugger
     // method to display 'guess-platform'
     // method to display 'guess-letters'
     
